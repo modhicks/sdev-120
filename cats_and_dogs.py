69e8a126a6f75b403c339fc77825b3980e7de49e
modhicks <moniquiahicks@gmail.com>
2025-02-14 23:46:58 -0500

    Add files via upload

---
+++ cats_and_dogs.py
@@ -0,0 +1,8 @@
+#Author: Moniquia Hicks
+#Date: 1/27/2025
+#Program: cats_and_dogs.py
+#Version: 1.0
+
+string_cats: str = "cats"
+string_dogs: str = "dogs"
+print("It's raining %s and %s!" % (string_cats, string_dogs))
